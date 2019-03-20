56167002a7ee2af1a23beea88bbad1d0b6241528
Marinel maxime <maxime.m62@gmail.com>
2019-03-20 13:20:21 +0100

    Remove client secret and id

--- wallabag/Controller/ClientIdViewController.swift
+++ wallabag/Controller/ClientIdViewController.swift
@@ -20,8 +20,6 @@ final class ClientIdViewController: UIViewController {
     override func viewDidLoad() {
         clientId.text = setting.get(for: .clientId)
         clientSecret.text = setting.get(for: .clientSecret)
-        clientId.text = "1_5ng39qhew3s4kc84ww00ck88wcowkgckc440skgss0wk8k4ooo"
-        clientSecret.text = "2kc4oo9ahgys0kko04sgc40oscks0g8kwcgsw0c0os0ss844g8"
         helpTextView.text = "Well, now the client_id and client_secret. This token are secret and can be found in your developer page, And create a new client or use an already existing token".localized
     }
 
